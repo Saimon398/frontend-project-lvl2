69e9ae28962d8616d545c2e4de55bf8970b58758
alex_p <aleksborislavskij@gmail.com>
2022-04-16 13:10:53 +0300

    start new project

---
+++ bin/gendiff.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env/node
+import { program } from 'commander';
+
+program
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .parse();
