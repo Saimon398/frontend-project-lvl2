0cf9e7a95c52e401919d34604edc3ce828834352
alex_p <aleksborislavskij@gmail.com>
2022-04-17 19:52:15 +0300

    added fixtures

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env/node
-import _ from 'lodash';
 import { program } from 'commander';
 
+
 program
-  .version('1.0.0')
+  .name('genDiff')
+  .version('1.0.0', '-v, --vers', 'output the current version')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format')
---
+++ src/utils.js
@@ -0,0 +1,19 @@
+import { resolve } from 'path';
+import { readFileSync } from 'fs';
+import { cwd } from 'process';
+
+/**
+ * Return an absolute path to a file
+ * @param {string} filename Name of file
+ * @returns {string} An absolute path
+ * @example
+ * getPath('file.json'); // users/anonimous/project/src/file.json
+ */
+export const getPath = (filename) => resolve(cwd(), filename);
+
+/**
+ * Read the entire contents of a file
+ * @param {string} filename Name of file
+ */
+export const readFileContent = (filename) => readFileSync(getPath(filename), 'utf-8');
+
