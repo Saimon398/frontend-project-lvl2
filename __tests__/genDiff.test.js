a4ce40207116fae7dea22b69d3281f14657a8109
alex_p <aleksborislavskij@gmail.com>
2022-04-19 14:30:46 +0300

    test:(__tests__/genDiff.test.js) added new tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,16 +1,19 @@
 import { test, expect } from '@jest/globals';
 import genDiff from '../src/index.js';
 
-const expected = `{
-  - follow: true
-  + follow: false
-  - host: hexlet.ru
-  + host: hexlet.io
-    proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 100
-}`;
 
-test('common flow', () => {
+let expected;
+
+test('main flow', () => {
+  // JSON formal
+  expected = genDiff('test1.json', 'test2.json');
   expect(genDiff('test1.json', 'test2.json')).toBe(expected);
+  // YAML format
+  expected = genDiff('test1.yaml', 'test2.yaml');
+  expect(genDiff('test1.yaml', 'test2.yaml')).toBe(expected);
+  // YML format
+  expected = genDiff('test1.yml', 'test2.yml');
+  expect(genDiff('test1.yml', 'test2.yml')).toBe(expected);
 });
+
+
