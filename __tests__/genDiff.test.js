d430f56e2ee407463fd3e805965ac4dc6c66c88e
alex_p <aleksborislavskij@gmail.com>
2022-04-20 15:28:43 +0300

    test:(__tests__/genDiff.test.js) added new test

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,19 +1,16 @@
-import { test, expect } from '@jest/globals';
+import { test, expect, describe } from '@jest/globals';
 import genDiff from '../src/index.js';
+import readFile from '../src/utils.js';
 
+const cases = [
+  ['test1.json', 'test2.json', 'result-json.txt'],
+  ['test1.yaml', 'test2.yaml', 'result-yaml.txt'],
+  ['test1.yml', 'test2.yml', 'result-yml.txt'],
+];
 
-let expected;
-
-test('main flow', () => {
-  // JSON formal
-  expected = genDiff('test1.json', 'test2.json');
-  expect(genDiff('test1.json', 'test2.json')).toBe(expected);
-  // YAML format
-  expected = genDiff('test1.yaml', 'test2.yaml');
-  expect(genDiff('test1.yaml', 'test2.yaml')).toBe(expected);
-  // YML format
-  expected = genDiff('test1.yml', 'test2.yml');
-  expect(genDiff('test1.yml', 'test2.yml')).toBe(expected);
+describe.each(cases)('compare %s and %s and show %s', (file1, file2, result) => {
+  test('checking common logic', () => {
+    const expected = readFile(result);
+    expect(genDiff(file1, file2)).toBe(expected);
+  });
 });
-
-
